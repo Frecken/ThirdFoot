e1db9dc82198fdabcd984bc04d8594524f9fa8a9
Dmitrij Rusakov <Daemon2017@gmail.com>
2017-05-10 03:05:53 +0300

    v0.0.1

---
+++ ThirdFoot/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ThirdFoot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        float[,] data = new float[50, 307200];
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp;
+
+            for (int n = 0; n < data.GetLength(0); n++)
+            {
+                bmp = new Bitmap(n + ".jpg");
+
+                pictureBox1.Image = bmp;
+                pictureBox1.Refresh();
+
+                bmp.RotateFlip(RotateFlipType.Rotate90FlipX);
+
+                pictureBox1.Image = bmp;
+                pictureBox1.Refresh();
+
+                int a = 0;
+
+                for (int x = 0; x < bmp.Width; ++x)
+                {
+                    for (int y = 0; y < bmp.Height; ++y)
+                    {
+                        Color curr = bmp.GetPixel(x, y);
+                        data[n, a] = curr.GetBrightness();
+                        a++;
+                    }
+                }
+
+                a = 0;
+            }
+
+            using (StreamWriter stream = new StreamWriter("Ideal_Input_Tactile.cfg"))
+            {
+                for (int i = 0; i < data.GetLength(0); i++)
+                {
+                    for (int k = 0; k < data.GetLength(1); k++)
+                    {
+                        stream.Write("{0};", data[i, k]);
+                    }
+                    stream.WriteLine();
+                }
+            }
+
+            data = null;
+        }
+    }
+}
---
+++ ThirdFoot/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ThirdFoot")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ThirdFoot")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("6dd3ffd1-0620-4426-b158-321e1f530093")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
